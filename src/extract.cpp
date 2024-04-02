c623707eaaf10f29a5c4863d049b65f4c313c09c
Jean-Luc Béchennec <jean-luc.bechennec@ls2n.fr>
2024-04-02 15:37:31 +0200

    ajout adc

--- src/extract.cpp
+++ src/extract.cpp
@@ -415,6 +415,28 @@ public:
   };
 };
 
+class ADC_t : public Inst_t {
+  uint8_t dReg, sReg;
+
+public:
+  ADC_t(const uint32_t inAddr, const uint16_t inCode) : Inst_t(inAddr) {
+    dReg = inCode & 0b111;
+    sReg = (inCode >> 3) & 0b111;
+  }
+  virtual void Print() { printf("%x: adcs r%d, r%d", addr, dReg, sReg); }
+
+  virtual void romeoFuncContent() {
+    printf("  uint64_t op1 = ");
+    pReg(dReg);
+    printf(";\n  uint64_t op2 = ");
+    pReg(dReg);
+    printf(";\n  uint64_t val = op1 + op2;\n");
+    wReg(dReg);
+    printf("val;\n");
+    updateSR("val", "op1", "op2");
+  };
+};
+
 class RSB_t : public Inst_t {
   uint8_t dReg, sReg;
 
@@ -569,6 +591,9 @@ Inst_t *Inst_t::decodeThumb2(const uint32_t inAddr, const uint16_t inCode) {
       switch (secondOpCode) {
       case 0:
         return new AND_t(inAddr, inCode);
+        break;
+      case 5:
+        return new ADC_t(inAddr, inCode);
         break;
       case 9:
         return new RSB_t(inAddr, inCode);
